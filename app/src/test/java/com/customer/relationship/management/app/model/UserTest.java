dba860a1fa0fc7db794a47876b9a7210793a0280
szymonbielowka <sbielowka@gmail.com>
2025-03-22 13:54:05 +0100

    database setup, dependency setup, User enitity added

---
+++ app/src/test/java/com/customer/relationship/management/app/model/UserTest.java
@@ -0,0 +1,26 @@
+package com.customer.relationship.management.app.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void shouldBeActiveByDefault() {
+        User newUser = new User();
+        assertTrue(newUser.isActive());
+    }
+
+    @Test
+    void shouldHaveCreatedDate() {
+        User newUser = new User();
+        assertNotNull(newUser.getCreatedAt());
+    }
+
+    @Test
+    void shouldHaveUpdatedDate() {
+        User newUser = new User();
+        assertNotNull(newUser.getUpdatedAt());
+    }
+}
